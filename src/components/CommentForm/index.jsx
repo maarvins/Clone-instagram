e8e743f5e9e028d658238fd08f0505f39a2dc53e
Marcus Sena <maarvins.19@gmail.com>
2021-10-28 12:11:25 -0300

    add icon smile

--- src/components/CommentForm/index.jsx
+++ src/components/CommentForm/index.jsx
@@ -1,8 +1,10 @@
 import "./styles.css"
+import {VscSmiley} from "react-icons/vsc"
 
 function CommentForm(props) {
   return (
     <div className="component-form-container" style={props.style}>
+      <VscSmiley size={30} />
       <input className="component-form-input" placeholder={props.placeholder} />
       <button
         style={props.buttonColor ? {color: props.buttonColor} : {}}
